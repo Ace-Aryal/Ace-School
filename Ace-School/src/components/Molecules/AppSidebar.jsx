300900430b2d5fae75d15a7eb95747fde1a88384
Ace-Aryal <aryaldeelep@gmail.com>
2025-04-16 13:11:03 +0545

    add role based sidebar

--- Ace-School/src/components/Molecules/AppSidebar.jsx
+++ Ace-School/src/components/Molecules/AppSidebar.jsx
@@ -1,4 +1,20 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Calendar,
+  Home,
+  Inbox,
+  Settings,
+  LayoutDashboard,
+  Coins,
+  ChartColumn,
+  Bell,
+  Users,
+  Pin,
+  GraduationCap,
+  Clock,
+  Library,
+  User,
+} from "lucide-react";
+
 import { useSidebar } from "@/components/ui/sidebar";
 import {
   Sidebar,
@@ -10,33 +26,80 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Link } from "react-router";
+import { useSelector } from "react-redux";
 
 // Menu items.
 const items = [
   {
-    title: "Home",
+    title: "Dashboard",
     url: "#",
-    icon: Home,
+    icon: LayoutDashboard,
+    readers: ["all"],
+    writers: ["none"],
+  },
+  {
+    title: "Fee Biling",
+    url: "#",
+    icon: Coins,
+    readers: ["accountant"],
+    writers: ["accountant"],
+  },
+  {
+    title: "Attendence",
+    url: "#",
+    icon: ChartColumn,
+    readers: ["accountant", "teacher", "admin", "principal"],
+    writers: ["accountant", "teacher", "admin", "principal"],
+  },
+  {
+    title: "Notices",
+    url: "#",
+    icon: Bell,
+    readers: ["all"],
+    writers: ["admin"],
   },
   {
     title: "Inbox",
     url: "#",
     icon: Inbox,
+    readers: ["admin"],
+    writers: ["none"],
   },
   {
     title: "Calendar",
     url: "#",
     icon: Calendar,
+    readers: ["all"],
+    writers: ["none"],
   },
   {
-    title: "Search",
+    title: "View Students",
     url: "#",
-    icon: Search,
+    icon: GraduationCap,
+    readers: ["accountant", "admin"],
+    writers: ["none"],
   },
   {
-    title: "Settings",
+    title: "View Teachers",
     url: "#",
-    icon: Settings,
+    icon: Users,
+    readers: ["accountant", "admin"],
+    writers: ["none"],
+  },
+  {
+    title: "Class Schedule",
+    url: "#",
+    icon: Clock,
+    readers: ["accountant", "teacher", "admin", "principal"],
+    writers: ["accountant", "admin", "principal"], // in every written place specify who wrote it in UI
+  },
+  {
+    title: "Library",
+    url: "#",
+    icon: Library,
+    readers: ["all"],
+    writers: ["accountant", "admin", "principal"],
   },
 ];
 
@@ -51,6 +114,8 @@ export default function AppSidebar() {
     toggleSidebar,
   } = useSidebar();
 
+  const { role, username } = useSelector((state) => state.auth.user);
+
   return (
     <Sidebar
       className="mt-14 z-1 sm:z-0 bg-indigo-100"
@@ -59,19 +124,29 @@ export default function AppSidebar() {
     >
       <SidebarContent className="bg-indigo-100">
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel className="text-md flex gap-1 font-semibold ">
+            <User size={64} />
+            {`${username} (${role})`}
+          </SidebarGroupLabel>
+
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {items
+                .filter(
+                  (element) =>
+                    element.readers.includes("all") ||
+                    element.readers.includes(role)
+                )
+                .map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link to={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
--- Ace-School/src/components/Organisms/DashboardPage.jsx
+++ Ace-School/src/components/Organisms/DashboardPage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Sidebar } from "../ui/sidebar";
-
-const DashboardPage = ({ role }) => {
+import { useSelector } from "react-redux";
+const DashboardPage = () => {
+  const { role, username } = useSelector((state) => state.auth.user);
   return (
-    <main id="container" className="m-0 p-0">
-      <section className="">HEllo</section>
+    <main
+      id="container"
+      className="m-0 p-0 flex flex-col items-center w-full mt-2 "
+    >
+      <h2 className="text-2xl text-center font-bold text-indigo-500 ">
+        Welcome {username}
+      </h2>
+      <section className="data-section flex flex-col my-4 self-start ">
+        <h3 className="text-xl">Dashboard</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div id="calender"></div>
+      </section>
     </main>
   );
 };
