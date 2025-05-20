f7ec529cc392f26e422cbf8c910b4ef78c58055c
Ace-Aryal <aryaldeelep@gmail.com>
2025-05-21 00:23:08 +0545

    test 11

--- Ace-School/functions/src/main.js
+++ Ace-School/functions/src/main.js
@@ -14,39 +14,39 @@ export default async ({ res, log }) => {
   );
 };
 
-  // const client = new Client()
-  //   .setEndpoint(process.env.APPWRITE_ENDPOINT)
-  //   .setProject(process.env.APPWRITE_PROJECT_ID)
-  //   .setKey(process.env.APPWRITE_API_KEY);
+// const client = new Client()
+//   .setEndpoint(process.env.APPWRITE_ENDPOINT)
+//   .setProject(process.env.APPWRITE_PROJECT_ID)
+//   .setKey(process.env.APPWRITE_API_KEY);
 
-  // const databases = new Databases(client);
+// const databases = new Databases(client);
 
-  // const inboxCleaner = databases.listDocuments(
-  //   process.env.DATABASE_ID,
-  //   process.env.INBOX_COLLECTION_ID
-  // );
-  // const noticeCleaner = databases.listDocuments(
-  //   process.env.DATABASE_ID,
-  //   process.env.NOTICE_COLLECTION_ID
-  // );
-  // try {
+// const inboxCleaner = databases.listDocuments(
+//   process.env.DATABASE_ID,
+//   process.env.INBOX_COLLECTION_ID
+// );
+// const noticeCleaner = databases.listDocuments(
+//   process.env.DATABASE_ID,
+//   process.env.NOTICE_COLLECTION_ID
+// );
+// try {
 
-  //   const [inboxResponse, noticeResponse] = await Promise.all([inboxCleaner, noticeCleaner])
-  //   const now = new Date();
-  //   const cutoff = new Date(now.getTime() - /*15 * 24 * 60 * */ 60 * 1000); // 15 day ago
+//   const [inboxResponse, noticeResponse] = await Promise.all([inboxCleaner, noticeCleaner])
+//   const now = new Date();
+//   const cutoff = new Date(now.getTime() - /*15 * 24 * 60 * */ 60 * 1000); // 15 day ago
 
-  //   for (const doc of inboxResponse.documents) {
-  //     const createdAt = new Date(doc.$createdAt);
+//   for (const doc of inboxResponse.documents) {
+//     const createdAt = new Date(doc.$createdAt);
 
-  //     if (createdAt < cutoff) {
-  //       await databases.deleteDocument(
-  //         process.env.DATABASE_ID,
-  //         process.env.INBOX_COLLECTION_ID,
-  //         doc.$id
-  //       );
-  //       log(`Deleted old document: ${doc.$id}`);
-  //     }
-}
+//     if (createdAt < cutoff) {
+//       await databases.deleteDocument(
+//         process.env.DATABASE_ID,
+//         process.env.INBOX_COLLECTION_ID,
+//         doc.$id
+//       );
+//       log(`Deleted old document: ${doc.$id}`);
+//     }
+// }
 
 //     for (const doc of noticeResponse.documents) {
 //       const createdAt = new Date(doc.$createdAt);
