46d12b0b8e9ea1b7341b4d7bf0858c6ae81d484e
Dipesh Aryal <aryaldeelep@gmail.com>
2025-06-20 09:54:39 +0545

    chore : add api key to env

--- Ace-School/appwrite/reset-att/main.js
+++ Ace-School/appwrite/reset-att/main.js
@@ -3,8 +3,8 @@ import { Client, Databases, Query } from "node-appwrite";
 export default async ({ req, res, log }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_ENDPOINT)
-    .setProject(process.env.APPWRITE_PROJECT_ID);
-
+    .setProject(process.env.APPWRITE_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
   const databases = new Databases(client);
 
   const databaseId = process.env.DATABASE_ID;
