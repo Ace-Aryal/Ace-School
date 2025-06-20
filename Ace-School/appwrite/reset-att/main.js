564bd20c4e13dd7f30e91f7468e03a60333e73d6
Dipesh Aryal <aryaldeelep@gmail.com>
2025-06-20 09:45:22 +0545

    fix : fix the error handling for attendance reset

--- Ace-School/appwrite/reset-att/main.js
+++ Ace-School/appwrite/reset-att/main.js
@@ -13,32 +13,38 @@ export default async ({ req, res, log }) => {
     process.env.TEACHER_ATT_COLLECTION,
     process.env.STAFF_ATT_COLLECTION,
   ];
+  try {
+    collectionIds.forEach(async (collection) => {
+      let offset = 0;
+      const limit = 100;
+      let totalFetched = 0;
+      let hasMore = true;
 
-  collectionIds.forEach(async (collection) => {
-    let offset = 0;
-    const limit = 100;
-    let totalFetched = 0;
-    let hasMore = true;
+      while (hasMore) {
+        const res = await databases.listDocuments(databaseId, collection, [
+          Query.limit(limit),
+          Query.offset(offset),
+        ]);
 
-    while (hasMore) {
-      const res = await databases.listDocuments(databaseId, collection, [
-        Query.limit(limit),
-        Query.offset(offset),
-      ]);
+        if (res.documents.length === 0) break;
 
-      if (res.documents.length === 0) break;
+        for (const doc of res.documents) {
+          await databases.updateDocument(databaseId, collection, doc.$id, {
+            attendance: "noattendance", // Example field
+          });
+        }
 
-      for (const doc of res.documents) {
-        await databases.updateDocument(databaseId, collection, doc.$id, {
-          attendance: "noattendance", // Example field
-        });
+        totalFetched += res.documents.length;
+        offset += limit;
+        hasMore = res.documents.length === limit; // If less than limit, we reached the end
       }
+    });
+    log("attendence reset complete");
+    return res.send("Attendence Reset complete");
+  } catch (error) {
+    log("Error updating attendance");
+    return res.send("Error reseting attendance");
+  }
 
-      totalFetched += res.documents.length;
-      offset += limit;
-      hasMore = res.documents.length === limit; // If less than limit, we reached the end
-    }
-  });
-
-  log("✅ Finished updating all documents.");
+  return res.send("✅ Finished updating all documents.");
 };
